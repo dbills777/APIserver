8e905d71cf9a70826cce06d6a6fa7b80f8edfc74
Derrick Bills <dbills777@gmail.com>
2020-10-02 12:10:56 -0600

    finish json only diplay for all methods

---
+++ data.js
@@ -0,0 +1,28 @@
+const players = [
+  {
+    id: 1,
+    name: 'Kobe',
+    team: 'Lakers',
+    rings: 5,
+  },
+  { id: 2, name: 'Olajuwon', team: 'Rockets', rings: 2 },
+  {
+    id: 3,
+    name: 'Rodman',
+    team: 'Bulls',
+    rings: 5,
+  },
+  {
+    id: 4,
+    name: 'Curry',
+    team: 'Warriors',
+    rings: 4,
+  },
+  {
+    id: 5,
+    name: 'Duncan',
+    team: 'spurs',
+    rings: 5,
+  },
+];
+module.exports = players;
